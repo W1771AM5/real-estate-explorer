60641bf8e52d001bf47ecf727345679248a7c148
Kerwin De Paz <kdepaz@qualtacon.com>
2026-08-28 15:51:15 -0700

    setting up cors

--- backend/RealEstate.Api/Program.cs
+++ backend/RealEstate.Api/Program.cs
@@ -1,7 +1,20 @@
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+{
+   options.AddPolicy(name: MyAllowSpecificOrigins,
+   policy =>
+   {
+       policy.WithOrigins("http://localhost:5173").AllowAnyHeader().AllowAnyMethod();
+
+   });
+});
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
@@ -43,7 +56,9 @@ app.MapGet("/api/health", () =>
 });
 
 
-
+app.UseCors(MyAllowSpecificOrigins);
+app.UseAuthorization();
+app.MapControllers();
 
 app.Run();
 
